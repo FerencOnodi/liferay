287a0860f29a5e80609b2fcb39ef7af150b7173d
FerencOnodi <onodiferenc91@gmail.com>
2021-03-08 20:34:28 +0100

    Update Tests.java
    Made changes that utilize updates from page classes

--- src/main/java/tests/Tests.java
+++ src/main/java/tests/Tests.java
@@ -7,17 +7,21 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.FormPage;
 import pages.SuccessPage;
 
 public class Tests {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "D:\\Programming\\Selenium\\chromeDriver\\chromeDriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Test
@@ -41,21 +45,44 @@ public class Tests {
         FormPage formPage = new FormPage(driver);
         driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548");
         formPage.enterText(formPage.nameInputField, "Test Name");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         formPage.clickOnElement(formPage.dateInputField);
         formPage.clickOnElement(formPage.previousMonthButton);
         formPage.clickOnElement(formPage.previousMonthButton);
-        formPage.clickOnElement(formPage.dayInCalendar);
-        Thread.sleep(1000);
+        formPage.clickOnElement(formPage.getDayInCalendar(15));
+        Thread.sleep(500);
         formPage.enterText(formPage.textArea, "Test reason");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         formPage.clickOnElement(formPage.submitButton);
-        Thread.sleep(1000);
 
         //Check information page contents
         SuccessPage successPage = new SuccessPage(driver);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(successPage.getElementByText("Information sent")));
         Assert.assertEquals("Information sent",  successPage.getValueOfH1Element());
         Assert.assertEquals("Information sent successfully!",  successPage.getValueOfH5Element());
+    }
+
+    @Test
+    public void testCase15() throws InterruptedException {
+        FormPage formPage = new FormPage(driver);
+        driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548");
+        formPage.changeLanguage();
+        Thread.sleep(500);
+        formPage.enterText(formPage.nameInputField, "El catos noir");
+        Thread.sleep(500);
+        formPage.clickOnElement(formPage.dateInputField);
+        formPage.clickOnElement(formPage.previousMonthButton);
+        formPage.clickOnElement(formPage.previousMonthButton);
+        formPage.clickOnElement(formPage.getDayInCalendar(13));
+        Thread.sleep(1000);
+        formPage.enterText(formPage.textArea, "Sí, mucho grande!");
+        Thread.sleep(1000);
+        formPage.clickOnElement(formPage.submitButton);
+
+        SuccessPage successPage = new SuccessPage(driver);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(successPage.getElementByText("Informações enviadas")));
+        Assert.assertEquals("Informações enviadas", successPage.getValueOfH1Element());
+        Assert.assertEquals("Informações enviadas com sucesso!", successPage.getValueOfH5Element());
     }
 
     @After
