2a1ac34a7d3517ddb8b2879254a5d66230b516b6
FerencOnodi <onodiferenc91@gmail.com>
2021-03-10 08:35:44 +0100

    Add chromedriver.exe
    Included chromedriver.exe file and updated Tests.java accordingly, also modified tcs

--- src/main/java/tests/Tests.java
+++ src/main/java/tests/Tests.java
@@ -5,12 +5,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.FormPage;
 import pages.SuccessPage;
+
+import java.util.List;
 
 public class Tests {
 
@@ -19,7 +22,7 @@ public class Tests {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "D:\\Programming\\Selenium\\chromeDriver\\chromeDriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
     }
@@ -31,15 +34,19 @@ public class Tests {
         formPage.clickOnElement(formPage.nameInputField);
         formPage.clickOnElement(formPage.dateInputField);
         formPage.clickOnElement(formPage.textArea);
-        Assert.assertEquals("This field is required.", formPage.getValue(formPage.feedbackForName));
-        Assert.assertEquals("This field is required.", formPage.getValue(formPage.feedbackForDate));
         formPage.clickOnElement(formPage.nameInputField);
-        Assert.assertEquals("This field is required.", formPage.getValue(formPage.feedbackForTextArea));
-        Assert.assertEquals("#f48989", Color.fromString(formPage.getCssPropertyValue(formPage.feedbackForName, "border-color")).asHex());
-        Assert.assertEquals("#feefef", Color.fromString(formPage.getCssPropertyValue(formPage.feedbackForName, "background-color")).asHex());
+        List<WebElement> feedbackList = driver.findElements(formPage.feedback);
+        Assert.assertEquals(3, feedbackList.size());
+        for (WebElement element : feedbackList) {
+            Assert.assertEquals("This field is required.", element.getText());
+        }
+        Assert.assertEquals("#f48989", Color.fromString(formPage.getCssPropertyValue(formPage.nameInputField, "border-color")).asHex());
+        Assert.assertEquals("#feefef", Color.fromString(formPage.getCssPropertyValue(formPage.nameInputField, "background-color")).asHex());
+
     }
 
     @Test
+    //Automated test for interview
     public void testCase14() throws InterruptedException {
         //Fill out input fields and click on Submit button
         FormPage formPage = new FormPage(driver);
@@ -63,22 +70,25 @@ public class Tests {
     }
 
     @Test
+    //Automated test for interview
     public void testCase15() throws InterruptedException {
+        //Fill out input fields and click on Submit button
         FormPage formPage = new FormPage(driver);
         driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548");
         formPage.changeLanguage();
         Thread.sleep(500);
-        formPage.enterText(formPage.nameInputField, "El catos noir");
+        formPage.enterText(formPage.nameInputField, "El catos negro");
         Thread.sleep(500);
         formPage.clickOnElement(formPage.dateInputField);
         formPage.clickOnElement(formPage.previousMonthButton);
         formPage.clickOnElement(formPage.previousMonthButton);
         formPage.clickOnElement(formPage.getDayInCalendar(13));
-        Thread.sleep(1000);
+        Thread.sleep(500);
         formPage.enterText(formPage.textArea, "Sí, mucho grande!");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         formPage.clickOnElement(formPage.submitButton);
 
+        //Check information page contents
         SuccessPage successPage = new SuccessPage(driver);
         wait.until(ExpectedConditions.visibilityOfElementLocated(successPage.getElementByText("Informações enviadas")));
         Assert.assertEquals("Informações enviadas", successPage.getValueOfH1Element());
